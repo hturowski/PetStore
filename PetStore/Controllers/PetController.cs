1b2e6a069ad7705d438ae59ba1bdf2154bf55049
Hank Turowski <hturowski@insight2profit.com>
2018-10-16 12:28:48 -0400

    Now with Post and Delete

--- PetStore/Controllers/PetController.cs
+++ PetStore/Controllers/PetController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PetStore.DAO;
+using System.Linq;
 using System.Collections;
 
 namespace PetStore
@@ -6,26 +8,66 @@ namespace PetStore
     [Route("pet")]
     public class PetController : Controller
     {
-        static Hashtable petDb = new Hashtable();
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            if (id == 1)
+            using (var db = new PetStoreContext())
             {
-                var result =
-                    Ok(new Pet(1, "Feynman", "Dog"));
-                return result;
-            }
-            else
-            {
-                return BadRequest();
+                try {
+                    var animal = db.Pets
+                        .Single(b => b.Id == id);
+                    return Ok(animal);
+                }
+                catch(System.Exception e)
+                {
+                    return NotFound(e);
+                }
             }
         }
 
         [HttpPut()]
-        public IActionResult Put()
+        public IActionResult Put(int id)
         {
-            petDb.Add("1", new Pet(1, "Feynman", "Dog"));
+            using (var db = new PetStoreContext())
+            {
+                db.Pets.Add(new Pet(2, "Zoey", "Cat"));
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Pet newPet)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            using (var db = new PetStoreContext())
+            {
+                db.Pets.Add(newPet);
+                db.SaveChanges();
+            }
+            return Ok(newPet);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new PetStoreContext())
+            {
+                try
+                {
+                    var toRemove = db.Pets.Single(p => p.Id == id);
+                    db.Pets.Remove(toRemove);
+                    db.SaveChanges();
+                }
+                catch(System.Exception e )
+                {
+                    return NotFound(e);
+                }
+            }
             return Ok();
         }
     }
