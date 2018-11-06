e9433e7b616d5f3a021b561e3edcd20fcc872963
Hank Turowski <hturowski@insight2profit.com>
2018-11-06 09:01:31 -0500

    Working on branch configs

--- PetStore/DAO/PetStoreContext.cs
+++ PetStore/DAO/PetStoreContext.cs
@@ -13,8 +13,11 @@ namespace PetStore.DAO
 	    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	    {
             var dbHost = Environment.GetEnvironmentVariable("DBHOST") ?? "localhost";
-            var dbName = Environment.GetEnvironmentVariable("DBNAME") ?? "pets";
-            var dbConnectionString = $"server={dbHost};database={dbName};user=root;password=root";
+            var dbName = Environment.GetEnvironmentVariable("DBNAME") ?? "petstore";
+            var dbUsername = Environment.GetEnvironmentVariable("DBUSERBAME") ?? "root";
+            var dbPassword = Environment.GetEnvironmentVariable("DBPASSWORD") ?? "root";
+
+            var dbConnectionString = $"server={dbHost};database={dbName};user={dbUsername};password={dbPassword}";
             System.Console.WriteLine(dbConnectionString);
             optionsBuilder.UseMySql(dbConnectionString);
 	    }
